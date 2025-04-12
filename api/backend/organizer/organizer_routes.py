48b0599e67ff5c1f28e13ea02c01f6400138d9f5
prooflesben <prooflesben@gmail.com>
2025-04-12 14:37:04 -0400

    Working

--- api/backend/organizer/organizer_routes.py
+++ api/backend/organizer/organizer_routes.py
@@ -37,22 +37,21 @@ def get_organizers():
         the_response.status_code = 500
     return the_response
 
+
+
 #------------------------------------------------------------
 # Get all non flagged reviews for an organizer with info like the organzier name and reviewer name
-@organizer.route('/organizers/<id>/events/reviews', methods=['GET'])
-def get_organizers_reviews(id):
-    print("getting the organizer reviews")
+@organizer.route('/organizers/<id>/contact-info', methods=['GET'])
+def get_organizers_contact_info(id):
+    print("getting the organizer contact info")
     try:
-        current_app.logger.info(f'GET /attendee/<id>/bookmarks route')
+        current_app.logger.info(f'GET organizers/<id>/contact-info')
 
         cursor = db.get_db().cursor()
         query = '''
-            SELECT orev.rating, orev.org_review_id, orev.comments, orev.written_by, O.name,A.first_name, A.last_name
-            FROM OrganizerReviews orev
-                JOIN Organizer O on O.organizer_id = orev.being_reviewed
-                JOIN Attendees A on A.attendee_id = orev.written_by
-            WHERE orev.flagged_by IS NULL AND orev.org_review_id = {0}
-            ORDER BY orev.being_reviewed
+            SELECT o.name, o.email, o.phone
+            FROM Organizer o
+            WHERE o.organizer_id = {0};
             '''
         cursor.execute(query.format(id))
         
@@ -67,4 +66,39 @@ def get_organizers_reviews(id):
         the_response.status_code = 500    
     
     return the_response
+
+
+#------------------------------------------------------------
+# Get all non flagged reviews for an organizer with info like the organzier name and reviewer name
+@organizer.route('/organizers/<id>/reviews', methods=['GET'])
+def get_organizers_reviews(id):
+    print("getting the organizer reviews")
+    try:
+        current_app.logger.info(f'GET /organizers/<id>/events/reviews')
+
+        cursor = db.get_db().cursor()
+        query = '''
+                SELECT orev.rating, orev.org_review_id, orev.comments, orev.written_by, O.name,A.first_name, A.last_name
+                FROM OrganizerReviews orev
+                    JOIN Organizer O on O.organizer_id = orev.being_reviewed
+                    JOIN Attendees A on A.attendee_id = orev.written_by
+                WHERE orev.flagged_by IS NULL AND orev.org_review_id = {0}
+                ORDER BY orev.being_reviewed;
+                '''
+        cursor.execute(query.format(id))
+        
+        theData = cursor.fetchall()
+        
+        the_response = make_response(jsonify(theData))
+        the_response.status_code = 200
+    except Exception as error:
+        print(error)      
+        the_response = make_response()  
+        the_response.status_code = 500    
+    
+    return the_response
+
+
+
+
 
