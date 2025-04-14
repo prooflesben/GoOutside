168a8106a14d9c4a2377faf6aebe384afbaca265
miayim <miayim@gmail.com>
2025-04-13 21:21:40 -0400

    added try except

--- api/backend/attendee/attendee_routes.py
+++ api/backend/attendee/attendee_routes.py
@@ -16,46 +16,53 @@ attendee = Blueprint('attendee', __name__)
 # Get all bookmarks for an attendee
 @attendee.route('/attendee/<id>/bookmarks', methods=['GET'])
 def get_attendee_bookmarks(id):
-    current_app.logger.info(f'GET /attendee/<id>/bookmarks route')
+    try:
+        cursor = db.get_db().cursor()
+        query = '''
+            SELECT e.event_id, e.event_name, e.event_date, e.event_location
+            FROM event e
+            JOIN event_bookmarks eb ON e.event_id = eb.event_id
+            JOIN attendee a ON eb.attendee_id = a.attendee_id
+            WHERE e.approved_by IS NOT NULL
+            ORDER BY e.event_date DESC
+            '''
+        cursor.execute(query, (id,))
+        
+        theData = cursor.fetchall()
+        
+        the_response = make_response(jsonify(theData))
+        the_response.status_code = 200
+        return the_response
+    except Exception as e:
+        current_app.logger.error(f"Error fetching attendee bookmarks: {e}")
 
-    cursor = db.get_db().cursor()
-    query = '''
-        SELECT e.event_id, e.event_name, e.event_date, e.event_location
-        FROM event e
-        JOIN event_bookmarks eb ON e.event_id = eb.event_id
-        JOIN attendee a ON eb.attendee_id = a.attendee_id
-        WHERE e.approved_by IS NOT NULL
-        ORDER BY e.event_date DESC
-        '''
-    cursor.execute(query, (id,))
-    
-    theData = cursor.fetchall()
-    
-    the_response = make_response(jsonify(theData))
-    the_response.status_code = 200
     return the_response
 
 #------------------------------------------------------------
 # Get all events an attendee has rsvpd to
 @attendee.route('/attendee/<id>/rsvps', methods=['GET'])
 def get_attendee_rsvps(id):
-    current_app.logger.info(f'GET /attendee/<id>/rsvps route')
+    try:
+        current_app.logger.info(f'GET /attendee/<id>/rsvps route')
 
-    cursor = db.get_db().cursor()
-    query = '''
-        SELECT e.event_id, e.event_name, e.event_date, e.event_location
-        FROM Events e
-        JOIN Event_Attendance er ON e.event_id = er.event_id
-        JOIN Attendee a ON er.attendee_id = a.attendee_id
-        WHERE e.approved_by IS NOT NULL
-        ORDER BY e.event_date DESC
-        '''
-    cursor.execute(query, (id,))
-    
-    theData = cursor.fetchall()
-    
-    the_response = make_response(jsonify(theData))
-    the_response.status_code = 200
+        cursor = db.get_db().cursor()
+        query = '''
+            SELECT e.event_id, e.event_name, e.event_date, e.event_location
+            FROM Events e
+            JOIN Event_Attendance er ON e.event_id = er.event_id
+            JOIN Attendee a ON er.attendee_id = a.attendee_id
+            WHERE e.approved_by IS NOT NULL
+            ORDER BY e.event_date DESC
+            '''
+        cursor.execute(query, (id,))
+        
+        theData = cursor.fetchall()
+        
+        the_response = make_response(jsonify(theData))
+        the_response.status_code = 200
+    except Exception as e:
+        current_app.logger.error(f"Error fetching attendee rsvps: {e}")
+
     return the_response
 
 #------------------------------------------------------------
