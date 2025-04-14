aabc5314989f3a4d109d20bf1c35de28a1044a13
miayim <miayim@gmail.com>
2025-04-13 21:25:34 -0400

    clean up code

--- api/backend/attendee/attendee_routes.py
+++ api/backend/attendee/attendee_routes.py
@@ -64,22 +64,3 @@ def get_attendee_rsvps(id):
         current_app.logger.error(f"Error fetching attendee rsvps: {e}")
 
     return the_response
-
-#------------------------------------------------------------
-# Update customer info for customer with particular userID
-#   Notice the manner of constructing the query.
-@attendee.route('/customers', methods=['PUT'])
-def update_customer():
-    current_app.logger.info('PUT /customers route')
-    cust_info = request.json
-    cust_id = cust_info['id']
-    first = cust_info['first_name']
-    last = cust_info['last_name']
-    company = cust_info['company']
-
-    query = 'UPDATE customers SET first_name = %s, last_name = %s, company = %s where id = %s'
-    data = (first, last, company, cust_id)
-    cursor = db.get_db().cursor()
-    r = cursor.execute(query, data)
-    db.get_db().commit()
-    return 'customer updated!'
