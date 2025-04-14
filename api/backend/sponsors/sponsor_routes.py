bb1a5c7bca370217b9434c740b6ddcaaedccc306
ryaken-nakamoto <ryakenNakamoto@gmail.com>
2025-04-13 21:17:58 -0400

    error msgs

--- api/backend/sponsors/sponsor_routes.py
+++ api/backend/sponsors/sponsor_routes.py
@@ -37,9 +37,7 @@ def get_sponsors():
 # will return sponsor ids and names from this, will pass thru via json
 @sponsors.route('/', methods=['POST'])
 def post_sponsor():
-    
     current_app.logger.info(f'POST /sponsors route')
-
     try:
         # get data
         the_data = request.json
@@ -73,101 +71,115 @@ def post_sponsor():
 # adds the sponsor of a given event
 @sponsors.route('/<int:sponsor_id>/events/<int:event_id>', methods=['PUT'])
 def link_sponsor(sponsor_id, event_id):
-    current_app.logger.info(f'PUT link /sponsors/{sponsor_id}/events/{event_id} route')
-
-    cursor = db.get_db().cursor()
-    query = '''
-        UPDATE Events
-        SET sponsor_by = %s
-        WHERE event_id = %s
-    '''
-    cursor.execute(query, (sponsor_id, event_id))
-    # saves the modification
-    db.get_db().commit()
-    the_response = make_response(jsonify({'message': 'Sponsor updated'}))
-    the_response.status_code = 200
+    try:
+        current_app.logger.info(f'PUT link /sponsors/{sponsor_id}/events/{event_id} route')
+        cursor = db.get_db().cursor()
+        query = '''
+            UPDATE Events
+            SET sponsor_by = %s
+            WHERE event_id = %s
+        '''
+        cursor.execute(query, (sponsor_id, event_id))
+        # saves the modification
+        db.get_db().commit()
+        the_response = make_response(jsonify({'message': 'Sponsor updated'}))
+        the_response.status_code = 200
+    except Exception as error:
+        print(error)      
+        response = make_response()  
+        response.status_code = 500
     return the_response
+    
 
 # removes the sponsor of a given event
 @sponsors.route('/<int:sponsor_id>/events/<int:event_id>', methods=['DELETE'])
 def unlink_sponsor(sponsor_id, event_id):
-    current_app.logger.info(f'DELETE /sponsors/{sponsor_id}/events/{event_id} route')
-
-    cursor = db.get_db().cursor()
-    query = '''
-        UPDATE Events
-        SET sponsor_by = NULL
-        WHERE event_id = %s
-    '''
-    cursor.execute(query, (event_id,))
-    db.get_db().commit()
-    return make_response(jsonify({'message': 'Sponsor removed'}), 200)
+    try:
+        current_app.logger.info(f'DELETE /sponsors/{sponsor_id}/events/{event_id} route')
+        cursor = db.get_db().cursor()
+        query = '''
+            UPDATE Events
+            SET sponsor_by = NULL
+            WHERE event_id = %s
+        '''
+        cursor.execute(query, (event_id,))
+        db.get_db().commit()
+        response = make_response(jsonify({'message': 'Sponsor removed'}), 200)
+    except Exception as error:
+        print(error)      
+        response = make_response()  
+        response.status_code = 500
+    return response
 
 
 # Gets all of the sponsors that have a review rating higher than a certain amount
 @sponsors.route('/reviews/<int:min_rating>', methods=['GET'])
 def get_filtered_sponsor_reviews(min_rating):
-    current_app.logger.info(f'GET /reviews<int: min_rating>')
-
-    cursor = db.get_db().cursor()
-    query = '''
-        SELECT s.name AS sponsor_name, AVG(sr.rating) AS avg_rating
-        FROM Sponsors s
-        JOIN SponsorReviews sr ON s.sponsor_id = sr.being_reviewed
-        GROUP BY s.sponsor_id
-        HAVING AVG(sr.rating) >= %s
-        ORDER BY avg_rating DESC;
-        '''
-    cursor.execute(query, (min_rating))
-    theData = cursor.fetchall()
-    the_response = make_response(jsonify(theData))
-    the_response.status_code = 200
-    return the_response
+    try:
+        current_app.logger.info(f'GET /reviews<int: min_rating>')
+        cursor = db.get_db().cursor()
+        query = '''
+            SELECT s.name AS sponsor_name, AVG(sr.rating) AS avg_rating
+            FROM Sponsors s
+            JOIN SponsorReviews sr ON s.sponsor_id = sr.being_reviewed
+            GROUP BY s.sponsor_id
+            HAVING AVG(sr.rating) >= %s
+            ORDER BY avg_rating DESC;
+            '''
+        cursor.execute(query, (min_rating))
+        theData = cursor.fetchall()
+        response = make_response(jsonify(theData))
+        response.status_code = 200
+    except Exception as error:
+        print(error)      
+        response = make_response()  
+        response.status_code = 500
+    return response
 
 
 # will add a review for a sponsor from an organizer
 @sponsors.route('/reviews', methods=['POST'])
 def add_organizer_review_on_sponsor():
-    current_app.logger.info('POST /reviews route')
+    try:
+        current_app.logger.info('POST /reviews route')
+        the_data = request.json
+        current_app.logger.info(f'Received data: {the_data}')
+        rating = the_data['rating']
+        sponsor_id = the_data['sponsor_id']
+        organizer_id = the_data['organizer_id']
+        comments = the_data.get('comments', '')
 
-    the_data = request.json
-    current_app.logger.info(f'Received data: {the_data}')
-    # extract fields
-    rating = the_data['rating']
-    sponsor_id = the_data['sponsor_id']
-    organizer_id = the_data['organizer_id']
-    # this is optional
-    comments = the_data.get('comments', '')
+        query = '''
+            INSERT INTO SponsorReviews (rating, written_by, being_reviewed, comments)
+            VALUES (%s, %s, %s, %s)
+        '''
+        
+        cursor = db.get_db().cursor()
+        cursor.execute(query, (rating, organizer_id, sponsor_id, comments))
+        db.get_db().commit()
 
-    # insert the review
-    query = '''
-        INSERT INTO SponsorReviews (rating, written_by, being_reviewed, comments)
-        VALUES (%s, %s, %s, %s)
-    '''
-    
-    cursor = db.get_db().cursor()
-    cursor.execute(query, (rating, organizer_id, sponsor_id, comments))
-    db.get_db().commit()
+        review_id = cursor.lastrowid
 
-    review_id = cursor.lastrowid
-
-    response = make_response(jsonify({
-        "message": "Review successfully created",
-        "review_id": review_id,
-        "rating": rating,
-        "sponsor_id": sponsor_id,
-        "organizer_id": organizer_id,
-        "comments": comments
-    }))
-    response.status_code = 200
+        response = make_response(jsonify({
+            "message": "Review successfully created",
+            "review_id": review_id,
+            "rating": rating,
+            "sponsor_id": sponsor_id,
+            "organizer_id": organizer_id,
+            "comments": comments
+        }))
+        response.status_code = 200
+    except Exception as error:
+        print(error)      
+        response = make_response()  
+        response.status_code = 500
     return response
 
-# Retreives all of the reviews for a sposnor
+# retreives all of the reviews for a sposnor
 @sponsors.route('/<int:sponsor_id>/reviews', methods=['GET'])
 def get_sponsor_reviews(sponsor_id):
-    current_app.logger.info(f'GET /sponsors/:id/reviews route')
-
     try:
+        current_app.logger.info(f'GET /sponsors/:id/reviews route')
         cursor = db.get_db().cursor()
         query = '''
             SELECT *
@@ -177,11 +189,10 @@ def get_sponsor_reviews(sponsor_id):
         cursor.execute(query.format(sponsor_id))
         theData = cursor.fetchall()
         
-        the_response = make_response(jsonify(theData))
-        the_response.status_code = 200
+        response = make_response(jsonify(theData))
+        response.status_code = 200
     except Exception as error:
         print(error)      
-        the_response = make_response()  
-        the_response.status_code = 500  
-
-    return the_response
+        response = make_response()  
+        response.status_code = 500
+    return response
