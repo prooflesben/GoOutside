1009341262c63b46bcf92a6cf075c1a005ac5c41
miayim <miayim@gmail.com>
2025-04-15 20:59:42 -0400

    tested post and delete for bookmarks

--- api/backend/attendee/attendee_routes.py
+++ api/backend/attendee/attendee_routes.py
@@ -19,13 +19,12 @@ def get_attendee_bookmarks(id):
 
     cursor = db.get_db().cursor()
     query = '''
-        SELECT e.event_id, e.event_name, e.event_date, e.event_location
-        FROM event e
-        JOIN event_bookmarks eb ON e.event_id = eb.event_id
-        JOIN attendee a ON eb.attendee_id = a.attendee_id
-        WHERE a.attendee_id = %s
+        SELECT e.event_id, e.name, e.start_time, e.location
+        FROM Events e
+        JOIN Event_Bookmarks eb ON e.event_id = eb.event_id
+        JOIN Attendees a ON eb.attendee_id = a.attendee_id
         WHERE e.approved_by IS NOT NULL
-        AND a.attendee_id = %s
+        AND eb.attendee_id = %s
         ORDER BY e.start_time DESC
         '''
     cursor.execute(query, (id,))
@@ -39,13 +38,13 @@ def get_attendee_bookmarks(id):
 
 #------------------------------------------------------------
 # Add a new event bookmark for an attendee
-@attendee.route('/attendees/<id>/bookmarks/<eventId>', methods=['POST'])
+@attendee.route('/<id>/bookmarks/<eventId>', methods=['POST'])
 def add_attendee_bookmark(id, eventId):
     current_app.logger.info(f'POST /attendee/<id>/bookmarks/<eventId> route')
 
     cursor = db.get_db().cursor()
     query = '''
-        INSERT INTO event_bookmarks (event_id, attendee_id)
+        INSERT INTO Event_Bookmarks (event_id, attendee_id)
         VALUES (%s, %s)
         '''
     cursor.execute(query, (eventId, id))
@@ -59,13 +58,13 @@ def add_attendee_bookmark(id, eventId):
 
 #------------------------------------------------------------
 # Delete a new event bookmark for an attendee
-@attendee.route('/attendees/<id>/bookmarks/<eventId>', methods=['DELETE'])
+@attendee.route('/<id>/bookmarks/<eventId>', methods=['DELETE'])
 def delete_attendee_bookmark(id, eventId):
     current_app.logger.info(f'DELETE /attendee/<id>/bookmarks/<eventId> route')
 
     cursor = db.get_db().cursor()
     query = '''
-        DELETE FROM event_bookmarks
+        DELETE FROM Event_Bookmarks
         WHERE event_id = %s AND attendee_id = %s
         '''
     cursor.execute(query, (eventId, id))
